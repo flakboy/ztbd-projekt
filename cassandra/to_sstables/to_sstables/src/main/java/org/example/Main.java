cd1b7fdb0c86dad3bea6e76df6b980a6586036d8
Kacper Mazurczyk <kacper1609@wp.pl>
2025-05-25 15:38:38 +0200

    adjustments in the csv to sstable converter

--- cassandra/to_sstables/to_sstables/src/main/java/org/example/Main.java
+++ cassandra/to_sstables/to_sstables/src/main/java/org/example/Main.java
@@ -29,12 +29,15 @@ public class Main {
         String insertStmt = "INSERT INTO yelp.reviews (review_id, user_id, business_id, review_text, date_reviewed) VALUES (?, ?, ?, ?, ?)";
 
         String dataPath = System.getenv().get("DATA_DIR");
-        String outputDirPath = dataPath + File.separator + "sstable";
+//        String outputDirPath = dataPath + File.separator + "sstable";
+        String outputDirPath = "/home/kali/sstable";
         org.apache.cassandra.io.util.File outputDir = new org.apache.cassandra.io.util.File(outputDirPath);
 
         String inputPath = dataPath + File.separator + "csv" + File.separator + "reviews.csv";
         System.out.println(inputPath);
 
+        int i = 0;
+        int milestoneInterval = 10_000;
         try (
             CQLSSTableWriter writer = CQLSSTableWriter.builder()
                 .inDirectory(outputDir)
@@ -47,18 +50,25 @@ public class Main {
                     .withCSVParser(new CSVParserBuilder()
                             .withSeparator('|')
                             .build()
-                    ).build();
+                    ).build()
         ) {
             //skip first line since it contains header
-            String[] row = reader.readNext();
+            String[] row;
+            reader.readNext();
             while ((row = reader.readNext()) != null) {
                 LocalDateTime review_date = LocalDateTime.parse(row[4]);
                 writer.addRow(row[0], row[1], row[2], row[3], new Date(review_date.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()));
+                i++;
+                if (i % milestoneInterval == 0) {
+                    System.out.println(String.format("Parsed %d rows.", i));
+                }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e);
+//            throw new RuntimeException(e);
         } catch (CsvValidationException e) {
-            throw new RuntimeException(e);
+            System.out.println(e);
+//            throw new RuntimeException(e);
         }
     }
 }
